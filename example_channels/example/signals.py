8b8f68b91cd0b55ffa1e33f86faa50cc0052c39a
Mohamed Magde Mohamed <mohamedmagdemohamed@MacBook-Pro.local>
2020-08-04 13:07:15 +0300

    first commit

---
+++ example_channels/example/signals.py
@@ -0,0 +1,12 @@
+from django.dispatch import receiver
+from example.models import LoggedInUser
+
+
+@receiver(user_logged_in)
+def on_user_login(sender, **kwargs):
+    LoggedInUser.objects.get_or_create(user=kwargs.get('user'))
+
+
+@receiver(user_logged_out)
+def on_user_logout(sender, **kwargs):
+    LoggedInUser.objects.filter(user=kwargs.get('user')).delete()
